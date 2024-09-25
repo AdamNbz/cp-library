b1eaf81bd9162d2dbeb8853e4240ebb49c2be74f
HOME <home@192.168.1.7>
2024-09-25 14:15:16 +0700

    templates

---
+++ fenwick.cpp
@@ -0,0 +1,95 @@
+/*
+Author : Nguyen Vo Ngoc Bao
+School : University of Information Technology, VNU-HCM
+*/
+#include <bits/stdc++.h>
+
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+
+#define fi first
+#define se second
+#define el '\n'
+#define pb push_back
+#define eb emplace_back
+#define io() ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+#define mask(i) (1LL << (i))
+#define getbit(mask, i) (((mask) >> (i)) & 1)
+
+using namespace std;
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> p32;
+typedef pair<ll, ll> p64;
+typedef pair<double, double> pdd;
+typedef vector<int> v32;
+typedef vector<vector<int> > vv32;
+typedef vector<ll> v64;
+typedef vector<vector<ll> > vv64;
+typedef vector<p32> vp32;
+typedef vector<vector<p32> > vvp32;
+typedef vector<p64> vp64;
+typedef vector<vector<p64> > vvp64;
+typedef vector<bool> vb;
+typedef vector<vector<bool> > vvb;
+typedef vector<string> vs;
+typedef vector<vs > vvs;
+
+int pop_cnt(ll mask) { return __builtin_popcountll(mask); }
+int ctz(ull mask) { return __builtin_ctzll(mask); }
+int logOf(ull mask) { return 63 - __builtin_clzll(mask); }
+
+const ll mod = (ll)(1e9+7);
+const ll inf = (ll)(2e18);
+
+template <typename T>
+class fenwick {
+public:
+    vector<T> fw;
+    int n;
+
+    // Constructor for initialization of size 
+    fenwick(int _n) : n(_n + 1) 
+    {
+        fw.resize(n);
+    }
+
+    // to modify , add or replace value at index x
+    void modify(int x, T v) 
+    {
+        while (x <= n) 
+        {
+            fw[x] += v;
+            x += (x & -x);
+        }
+    }
+
+    // To get sum of prefix ending at x
+    T get(int x) 
+    {
+        T v{};
+        while (x > 0) 
+        {
+            v += fw[x];
+            x -= (x & -x);
+        }
+        return v;
+    }
+};
+
+void sol()
+{
+    
+}
+
+int32_t main()
+{
+    io();
+    int t; cin >> t;
+    while (t--) sol();
+    cerr << "\nTime elapsed: " << 1000*clock()/CLOCKS_PER_SEC << "ms\n";
+}
